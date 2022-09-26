a940c9c6fe0388ae05ae4946c046cee5c5bd471a
jann83 <jannlg83@gmail.com>
2022-09-25 21:36:25 -0500

    25092022

---
+++ js/script.js
@@ -0,0 +1,31 @@
+ 
+//variables  
+let nombre = "juan ";
+
+console.log(nombre);
+
+let producto; //variable sin valor 
+
+
+producto = true;
+
+//declaración de varias variables, 
+
+var producto1 = 'computadora',
+    disponible1 = true,
+    categoria = 'computadora';
+
+    //las variables no pueden iniciar con múmeros o simbolo
+
+    // var 1disponible = 'jann'
+    // var -estilos = 'nombre '
+
+
+    var nombre_producto = 'monitor HD'; //método se le conoce como underscore 
+    var nombreProducto = 'Monitor Hd';//conocido como camelcase
+    var NombreProducto = 'monitor hd'; //pascalcase
+    var nombreproducto = 'monitor hd'; //lowercase
+
+
+    console.log(producto);
+    
